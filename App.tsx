89e6dfdd0897bf45fd369f21807fdc945ffc60f2
talentceffort <talentceffort@gmail.com>
2020-06-11 20:16:34 +0900

    init BottmTab

--- App.tsx
+++ App.tsx
@@ -1,35 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ApolloProvider } from 'react-apollo-hooks';
 import ApolloClient from 'apollo-boost';
 import apolloClientOptions from './apollo';
+import ProjectScreen from './screens/Project';
+import HomeScreen from './screens/Home';
+import AddProjectScreen from './screens/AddProject';
+import ShowOffScreen from './screens/ShowOff';
+import MyProfileScreen from './screens/MyProfile';
 
 const client = new ApolloClient({
   ...apolloClientOptions,
 });
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
   return (
   <ApolloProvider client = {client}>
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name='홈' component={HomeScreen} />
+        <Tab.Screen name='프로젝트' component={ProjectScreen} />
+        <Tab.Screen name='만들기' component={AddProjectScreen} />
+        <Tab.Screen name='자랑하기' component={ShowOffScreen} />
+        <Tab.Screen name='마이' component={MyProfileScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   </ApolloProvider>
-
   );
 }
 
