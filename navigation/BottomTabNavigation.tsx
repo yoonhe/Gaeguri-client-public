1f2f17688a1828dff63799cfbe638601f941e2f3
talentceffort <47441575+talentceffort@users.noreply.github.com>
2020-06-16 20:23:55 +0900

    Update BottomTabNavigation.tsx

--- navigation/BottomTabNavigation.tsx
+++ navigation/BottomTabNavigation.tsx
@@ -10,12 +10,12 @@ function BottomTabNavigation(): React.ReactElement {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator initialRouteName="홈">
-      <Tab.Screen name="홈" component={HomeScreen} />
-      <Tab.Screen name="프로젝트" component={ProjectStack} />
-      <Tab.Screen name="만들기" component={CreateProjectStack} />
-      <Tab.Screen name="자랑하기" component={ShowOffScreen} />
-      <Tab.Screen name="마이" component={MyProfileScreen} />
+    <Tab.Navigator initialRouteName='홈'>
+      <Tab.Screen name='홈' component={HomeScreen} />
+      <Tab.Screen name='프로젝트' component={ProjectStack} />
+      <Tab.Screen name='만들기' component={CreateProjectStack} />
+      <Tab.Screen name='자랑하기' component={ShowOffScreen} />
+      <Tab.Screen name='마이' component={MyProfileScreen} />
     </Tab.Navigator>
   );
 }
