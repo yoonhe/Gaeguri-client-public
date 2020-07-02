1456bf579d0e4a9aa7fdadc3e37914fc5a6e3da0
yoonhe <yhehe228@gmail.com>
2020-07-02 23:47:03 +0900

    create project list page

--- styles/tag.ts
+++ styles/tag.ts
@@ -4,25 +4,25 @@ export const TagListStyle = styled.View`
   display: flex;
   flex-wrap: wrap;
   flex-direction: row;
-  margin: 5px -5px -5px;
+  margin: ${props => (props.type === 'text' ? '0' : '5px -5px -5px')};
 `;
 
 export const TagItemStyle = styled.View`
   position: relative;
   flex-direction: row;
-  margin: 5px;
-  padding: 10px 0 10px 10px;
-  color: #5dd7b9;
-  border: 1px solid #5dd7b9;
+  margin: ${props => (props.type === 'text' ? '0 10px 0 0' : '5px')};
+  padding: ${props => (props.type === 'text' ? '0' : '10px 0 10px 10px')};
+  border: ${props => (props.type === 'text' ? 'none' : '1px solid #5dd7b9')};
   border-radius: 15px;
   background: #fff;
   overflow: hidden;
 `;
 
 export const TagTextStyle = styled.Text`
-  margin-right: 30px;
+  margin-right: ${props => (props.type === 'text' ? '0' : '30px')};
   align-items: flex-start;
-  color: #5dd7b9;
+  color: ${props => (props.type === 'text' ? '#8A929E' : '#5dd7b9')};
+  font-size: ${props => props.type === 'text' && '16px'};
 `;
 
 export const TagTextButtonStyle = styled.Text`
