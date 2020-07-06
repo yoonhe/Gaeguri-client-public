f40c41eb990da7813356aea994f15ed32589bbf6
talentceffort <talentceffort@gmail.com>
2020-07-06 22:22:39 +0900

    Apollo ws Setting Complete

--- App.tsx
+++ App.tsx
@@ -1,33 +1,9 @@
 import React from 'react';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
-import apolloClientOptions from './apollo';
 import StatusBarComponent from './components/StatusBarComponent';
 import BottomTabNavigation from './navigation/BottomTabNavigation';
 import { NavigationContainer } from '@react-navigation/native';
-import AsyncStorage from '@react-native-community/async-storage';
-const cache = new InMemoryCache();
-
-const client = new ApolloClient({
-  ...apolloClientOptions,
-  cache,
-  credentials: 'same-origin',
-  request: async operation => {
-    const token = await AsyncStorage.getItem('token');
-    operation.setContext({
-      headers: token
-        ? {
-            'X-JWT': `${token}`,
-          }
-        : {},
-    });
-  },
-  onError: ({ networkError, graphQLErrors }) => {
-    console.log('graphQLErrors', graphQLErrors);
-    console.log('networkError', networkError);
-  },
-});
+import client from './apollo';
 
 function App(): React.ReactElement {
   return (
--- apollo.ts
+++ apollo.ts
@@ -1,9 +1,69 @@
-interface Options {
-  uri: string;
-}
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink, concat, Operation, split } from 'apollo-link';
+import { onError } from 'apollo-link-error';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { toast } from 'react-toastify';
+import AsyncStorage from '@react-native-community/async-storage';
 
-const options: Options = {
-  uri: 'http://10.0.2.2:4000/graphql',
+const getToken = async () => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+    return token;
+  }
+  return '';
 };
 
-export default options;
+const cache = new InMemoryCache();
+
+const authMiddleware = new ApolloLink((operation: Operation, forward: any) => {
+  operation.setContext({
+    headers: {
+      'X-JWT': getToken(),
+    },
+  });
+  return forward(operation);
+});
+
+const httpLink = new HttpLink({
+  uri: 'http://52.78.76.186:4000/graphql',
+});
+
+const wsLink = new WebSocketLink({
+  options: {
+    connectionParams: {
+      'X-JWT': getToken(),
+    },
+    reconnect: true,
+  },
+  uri: 'ws://52.78.76.186:4000/subscription',
+});
+
+const combinedLinks = split(
+  ({ query }) => {
+    const { kind, operation }: any = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message }) => {
+      toast.error(`Unexpected error: ${message}`);
+    });
+  }
+  if (networkError) {
+    toast.error(`Network error: ${networkError}`);
+  }
+});
+
+const client = new ApolloClient({
+  cache,
+  link: ApolloLink.from([errorLink, concat(authMiddleware, combinedLinks)]),
+});
+
+export default client;
