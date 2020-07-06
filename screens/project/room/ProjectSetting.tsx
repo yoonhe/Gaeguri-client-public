316a25d72e5ad935ebcda74684666735340d4ff2
talentceffort <talentceffort@gmail.com>
2020-07-06 21:44:19 +0900

    Query Modify

--- screens/project/room/ProjectSetting.tsx
+++ screens/project/room/ProjectSetting.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useCallback } from 'react';
+import React, { useLayoutEffect, useCallback, useState } from 'react';
 import { View, Text, Button } from 'react-native';
 import FormBoxComponent from '../../../components/FormBoxComponent';
 import { useFormik } from 'formik';
@@ -6,12 +6,18 @@ import { PageWrapStyle } from '../../../styles/common';
 import gql from 'graphql-tag';
 import { useMutation, useQuery } from '@apollo/react-hooks';
 import { BorderButton } from '../../../components/ButtonComponent';
+import { ButtonWrap } from '../../../styles/button';
 
 const UPDATE_PROJECT = gql`
   mutation UpdateProjectInfo($input: upProject!) {
     updateProjectInfo(input: $input) {
-      message
-      path
+      ok
+      error
+      project {
+        Project_id
+        Project_name
+        status
+      }
     }
   }
 `;
@@ -19,22 +25,42 @@ const UPDATE_PROJECT = gql`
 const GET_PROJECT = gql`
   query GetProjectDetail($Project_id: Int!) {
     getProjectDetail(Project_id: $Project_id) {
+      Project_id
       Project_name
       status
     }
   }
 `;
 
-// 모집중: ‘await’ 시작: ‘Start’ 종료: ‘End’
 function ProjectSetting({ navigation }): React.ReactElement {
   const [updateProject] = useMutation(UPDATE_PROJECT, {
     refetchQueries: [{ query: GET_PROJECT, variables: { Project_id: 2 } }],
-    awaitRefetchQueries: true,
+    // awaitRefetchQueries: true,
   });
 
   const { loading, error, data } = useQuery(GET_PROJECT, {
     variables: { Project_id: 2 },
   });
+
+  console.log('render', data?.getProjectDetail);
+
+  const stateChangeToText = (status: number) => {
+    let statusString: string = '';
+    switch (status) {
+      case 0:
+        statusString = 'await';
+        break;
+      case 1:
+        statusString = 'Start';
+        break;
+      case 2:
+        statusString = 'End';
+        break;
+    }
+    return statusString;
+  };
+
+  const [buttonState, setButtonState] = useState<number>(0);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -59,18 +85,20 @@ function ProjectSetting({ navigation }): React.ReactElement {
           input: {
             Project_id: 2,
             Project_name: values.projectName,
+            // Status: stateChangeToText(buttonState),
           },
         },
       });
     },
   });
 
-  const onClick = useCallback(() => {
-    console.log('click');
+  const stateArr: string[] = ['모집중', '시작', '종료'];
+
+  const onClick = useCallback(e => {
+    setButtonState(e);
   }, []);
 
-  console.log(data?.getProjectDetail?.status);
-
+  // 모집중: ‘await’ 시작: ‘Start’ 종료: ‘End’
   return (
     <PageWrapStyle>
       <FormBoxComponent
@@ -79,12 +107,22 @@ function ProjectSetting({ navigation }): React.ReactElement {
         values={formik.values.projectName}
         onChangeText={formik.handleChange('projectName')}
       />
-      <Text>진행 상황</Text>
-      <View style={{ flexDirection: 'row', justifyContent: 'center', flex: 1 }}>
-        <BorderButton children='모집중' onPress={onClick} />
-        <BorderButton children='진행중' onPress={onClick} />
-        <BorderButton children='종료' onPress={onClick} />
-      </View>
+      <FormBoxComponent title='주요스택'>
+        <ButtonWrap>
+          {stateArr.map((state, i) => {
+            return (
+              <BorderButton
+                key={i}
+                row={true}
+                backgroundColor={buttonState === i}
+                onPress={() => onClick(i)}
+              >
+                {stateArr[i]}
+              </BorderButton>
+            );
+          })}
+        </ButtonWrap>
+      </FormBoxComponent>
     </PageWrapStyle>
   );
 }
