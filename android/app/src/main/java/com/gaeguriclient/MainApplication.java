57ad50df6e4c7085cf3dfe8d8211c5d8c7bdadd8
MAC <mac@ara.local>
2020-07-11 22:18:34 +0700

    ADD fb login

--- android/app/src/main/java/com/gaeguriclient/MainApplication.java
+++ android/app/src/main/java/com/gaeguriclient/MainApplication.java
@@ -4,10 +4,13 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.FacebookSdk;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
