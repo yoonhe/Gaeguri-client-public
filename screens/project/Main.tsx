ae9a4c232055487d94981f576031ffe51071b634
yoonhe <yhehe228@gmail.com>
2020-07-20 19:47:39 +0900

    After creating a project, go to the Home tab

--- screens/project/Main.tsx
+++ screens/project/Main.tsx
@@ -1,19 +1,11 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import { View, ActivityIndicator, ScrollView, Button } from 'react-native';
 import { useQuery } from '@apollo/react-hooks';
 import { PageWrapStyle } from '../../styles/common';
 import CardListComponent from '../../components/CardListComponent';
-// import ProjectStatusPickerComponent from '../../components/ProjectStatusPickerComponent';
 import { GET_PROJECT } from './ProjectQuries';
 
 function Main({ navigation }): React.ReactElement {
-  const [selectedValue, setSelectedValue] = useState('onGoing');
-  // const statusPickerOnChange = useCallback((itemValue, itemIndex) => {
-  //   console.log('itemValue', itemValue);
-  //   console.log('itemIndex', itemIndex);
-  //   setSelectedValue(itemValue);
-  // }, []);
-
   const { loading, error, data } = useQuery(GET_PROJECT, {
     pollInterval: 200,
   });
@@ -22,20 +14,18 @@ function Main({ navigation }): React.ReactElement {
     console.log('[CardListComponents] error ? ', error);
   }
 
+  console.log('[Main Project List] ', data);
+
   return (
     <PageWrapStyle>
       <ScrollView style={{ flex: 1 }}>
-        {/* <ProjectStatusPickerComponent
-          selectedValue={selectedValue}
-          onValueChange={statusPickerOnChange}
-        /> */}
         {loading && (
           <View>
             <ActivityIndicator size="small" color="#00ff00" />
           </View>
         )}
         {data &&
-          data.getAvailableProjectList.map((project, i) => (
+          data?.getAvailableProjectList?.map((project, i) => (
             <CardListComponent key={i} project={project} navigation={navigation} />
           ))}
       </ScrollView>
